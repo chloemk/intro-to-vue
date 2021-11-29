4a3753623b5ae2dc45f5c59b2c33b0617974cd15
chloemk <minsung.chloe.kim@gmail.com>
2021-11-30 00:31:34 +0900

    Add: vue-cli created

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<HelloWorld />
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+	//new Vue 인스턴스 만든 것과 같음. 인스턴스 옵션 속성 또는 컴포넌트 옵션 속성 넣어주면 됨.
+	name: 'app',
+	components: {
+		HelloWorld,
+		'hello-world': HelloWorld,
+	},
+};
+</script>
+
+<style></style>
